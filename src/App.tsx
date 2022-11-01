7fe307eda5522fa6d7af8b4cbb6abd2efa402506
Troy DeSpain <despaintroy@gmail.com>
2022-11-01 06:48:27 -0600

    Install MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import {Container, Typography} from "@mui/material"
 import React from "react"
 
 function App() {
-  return <div className="App">Hello World</div>
+  return (
+    <Container>
+      <Typography>Hello World</Typography>
+    </Container>
+  )
 }
 
 export default App
