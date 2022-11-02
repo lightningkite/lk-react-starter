9342b892a135b8cd80391d45700de8d837f96b0d
Troy DeSpain <despaintroy@gmail.com>
2022-11-02 11:34:28 -0600

    Break out user menu to separate component

--- src/Layouts/MainLayout/MainLayout.tsx
+++ src/Layouts/MainLayout/MainLayout.tsx
@@ -1,20 +1,7 @@
-import {AccountCircle, Logout} from "@mui/icons-material"
-import {
-  Avatar,
-  Box,
-  Button,
-  Divider,
-  ListItemAvatar,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Menu,
-  MenuItem,
-  Stack
-} from "@mui/material"
-import {AuthContext} from "App"
-import React, {FC, ReactNode, useContext, useState} from "react"
-import {NavLink, useNavigate} from "react-router-dom"
+import {Box, Button, Divider, Stack} from "@mui/material"
+import React, {FC, ReactNode} from "react"
+import {NavLink} from "react-router-dom"
+import MyUserMenu from "./MyUserMenu"
 
 // Define the items that will appear in the sidebar navigation
 const navItems: Array<{label: string; to: string}> = [
@@ -23,87 +10,44 @@ const navItems: Array<{label: string; to: string}> = [
 ]
 
 const MainLayout: FC<{children: ReactNode}> = ({children}) => {
-  const {logout, currentUser} = useContext(AuthContext)
-  const navigate = useNavigate()
-
-  const [userMenuAnchor, setUserMenuAnchor] = useState<null | HTMLElement>(null)
-  const closeMenu = (): void => setUserMenuAnchor(null)
-
   return (
-    <>
-      <Stack direction="row" minHeight="100vh">
-        <Box
-          sx={{
-            p: 1,
-            zIndex: 1,
-            backgroundColor: "white",
-            width: "20rem",
-            boxShadow: "0 3px 6px rgba(0,0,0,0.16)"
-          }}
-        >
-          <ListItemButton onClick={(e) => setUserMenuAnchor(e.currentTarget)}>
-            <ListItemAvatar>
-              <Avatar src={currentUser.profilePic} />
-            </ListItemAvatar>
-            <ListItemText
-              primary={currentUser.name}
-              secondary={currentUser.email}
-            />
-          </ListItemButton>
-
-          <Divider sx={{m: 2}} />
-
-          <Stack spacing={1}>
-            {navItems.map(({label, to}) => (
-              <Button
-                key={to}
-                component={NavLink}
-                to={to}
-                sx={{
-                  justifyContent: "start",
-                  "&.active": {
-                    bgcolor: "primary.main",
-                    color: "white"
-                  }
-                }}
-              >
-                {label}
-              </Button>
-            ))}
-          </Stack>
-        </Box>
-        <Box bgcolor="background.default" width="100%" pt={3} pb={7}>
-          {children}
-        </Box>
-      </Stack>
-
-      <Menu
-        anchorEl={userMenuAnchor}
-        open={!!userMenuAnchor}
-        onClose={closeMenu}
-        MenuListProps={{
-          "aria-labelledby": "basic-button"
+    <Stack direction="row" minHeight="100vh">
+      <Box
+        sx={{
+          p: 1,
+          zIndex: 1,
+          backgroundColor: "white",
+          width: "20rem",
+          boxShadow: "0 3px 6px rgba(0,0,0,0.16)"
         }}
       >
-        <MenuItem
-          onClick={() => {
-            closeMenu()
-            navigate(`/users/${currentUser._id}`)
-          }}
-        >
-          <ListItemIcon>
-            <AccountCircle fontSize="small" />
-          </ListItemIcon>
-          <ListItemText>My Account</ListItemText>
-        </MenuItem>
-        <MenuItem onClick={logout}>
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          <ListItemText>Logout</ListItemText>
-        </MenuItem>
-      </Menu>
-    </>
+        <MyUserMenu />
+
+        <Divider sx={{m: 2}} />
+
+        <Stack spacing={1}>
+          {navItems.map(({label, to}) => (
+            <Button
+              key={to}
+              component={NavLink}
+              to={to}
+              sx={{
+                justifyContent: "start",
+                "&.active": {
+                  bgcolor: "primary.main",
+                  color: "white"
+                }
+              }}
+            >
+              {label}
+            </Button>
+          ))}
+        </Stack>
+      </Box>
+      <Box bgcolor="background.default" width="100%" pt={3} pb={7}>
+        {children}
+      </Box>
+    </Stack>
   )
 }
 
