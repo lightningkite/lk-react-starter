c9952bb6d6ba80c24a76b1ab513792e174c8277a
Troy DeSpain <despaintroy@gmail.com>
2022-11-02 10:28:50 -0600

    Breadcrumbs

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,20 +1,46 @@
-import {Stack, Typography} from "@mui/material"
+import {Box, Breadcrumbs, Link, Stack, Typography} from "@mui/material"
 import React, {FC, PropsWithChildren} from "react"
+import {Link as RouterLink} from "react-router-dom"
+
+export type BreadCrumb = [label: string, path: string]
 
 export interface PageHeaderProps {
   title: string
+  breadcrumbs?: BreadCrumb[]
 }
 
 const PageHeader: FC<PropsWithChildren<PageHeaderProps>> = (props) => {
-  const {title, children} = props
+  const {title, breadcrumbs, children} = props
 
   return (
-    <Stack direction="row" alignItems="center" justifyContent="start">
-      <Typography variant="h1" sx={{mt: 3, mb: 3, mr: "auto"}}>
-        {title}
-      </Typography>
-      {children}
-    </Stack>
+    <Box mt={3} mb={3}>
+      {breadcrumbs && (
+        <Breadcrumbs sx={{mb: 2}} separator="›">
+          {breadcrumbs.map(([label, path], index) =>
+            index !== breadcrumbs.length - 1 ? (
+              <Link
+                component={RouterLink}
+                key={path}
+                underline="hover"
+                color="inherit"
+                to={path}
+              >
+                {label}
+              </Link>
+            ) : (
+              <Typography color="text.primary">{label}</Typography>
+            )
+          )}
+        </Breadcrumbs>
+      )}
+
+      <Stack direction="row" alignItems="center" justifyContent="start">
+        <Typography variant="h1" sx={{mr: "auto"}}>
+          {title}
+        </Typography>
+        {children}
+      </Stack>
+    </Box>
   )
 }
 
--- src/pages/UserDetail/UserDetail.tsx
+++ src/pages/UserDetail/UserDetail.tsx
@@ -1,4 +1,3 @@
-import {ArrowLeft} from "@mui/icons-material"
 import {Button, Card, CardContent, Container} from "@mui/material"
 import {User} from "api/sdk"
 import {AuthContext} from "App"
@@ -6,12 +5,11 @@ import ErrorAlert from "components/ErrorAlert"
 import Loading from "components/Loading"
 import PageHeader from "components/PageHeader"
 import React, {FC, useContext, useEffect, useState} from "react"
-import {useNavigate, useParams} from "react-router-dom"
+import {useParams} from "react-router-dom"
 import {UserForm} from "./UserForm"
 
 export const UserDetail: FC = () => {
   const {userId} = useParams()
-  const navigate = useNavigate()
   const {session} = useContext(AuthContext)
 
   const [user, setUser] = useState<User | null>()
@@ -35,17 +33,15 @@ export const UserDetail: FC = () => {
 
   return (
     <Container maxWidth="sm">
-      <PageHeader title={user.name}>
+      <PageHeader
+        title={user.name}
+        breadcrumbs={[
+          ["All Users", "/users"],
+          [user.name, ""]
+        ]}
+      >
         <Button color="error">Delete</Button>
       </PageHeader>
-
-      <Button
-        startIcon={<ArrowLeft />}
-        onClick={() => navigate("/users")}
-        sx={{mb: 2}}
-      >
-        All Users
-      </Button>
 
       <Card>
         <CardContent sx={{mt: 2}}>
